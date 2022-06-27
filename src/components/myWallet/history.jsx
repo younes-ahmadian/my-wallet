1cb5b1509b96511abd06ae9f60fd2779bc619616
younes <younes_ahmadian@yahoo.com>
2022-06-27 13:03:36 -0700

    revising url background

--- src/components/myWallet/history.jsx
+++ src/components/myWallet/history.jsx
@@ -8,7 +8,7 @@ const History = ({data, onClose, onEdit}) => {
         <div className="history">
             <div className='historyHeader'>History</div>
             <div className="historyContent">
-                {data.map(item => <div className={item.type === "addMoney" ? "increase" : "decrease"} key={item.id}> 
+                {data.map(item => <div className={item.type === "addMoney" ? "increase" : "decrease"} key={item.id}>
                 <span className='comment'>{item.comment}</span> <span className='quantity'>{item.quantity}
                 <span className='editIcon'><EditIcon onClick={() => onEdit(item)}/></span>
                 <span className='closeIcon'><Close onClick={() => onClose(item)}/></span></span>    
